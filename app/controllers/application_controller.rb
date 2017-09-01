2151436fe0de8c35569b5597b8d7621d15766403
Corey Shaw <shaw.corey@gmail.com>
2017-09-01 15:30:34 +0000

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+  end
+  
+end
